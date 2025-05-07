a1803ebb5b25e27bcb610bb24411d8589b9ba6cc
木泠 <clemooling@outlook.com>
2025-05-07 20:22:46 +0800

    feat: add base class 'Player' to identify any valid players.

--- player_api/__init__.py
+++ player_api/__init__.py
@@ -8,20 +8,31 @@ from uuid import UUID
 class Player:
     name: str
     uuid: UUID
-    online_mode: bool
+    online_mode: Optional[bool] = None
+    official_account: Optional[bool] = None
+    api: Optional[HttpUrl] = None
+    service_name: Optional[str] = None
 
+
+@dataclass
 class OfflinePlayer(Player):
     online_mode: bool = field(default=False, init=False)
+    official_account: bool = field(default=False, init=False)
+    api: Optional[HttpUrl] = field(default=None, init=False)
+    service_name: str = "offline"
 
+@dataclass
 class OnlinePlayer(Player):
     online_mode: bool = field(default=True, init=False)
     official_account: bool
-    api: Optional[HttpUrl]
-    service_name: Optional[str] # not actually works, just as a tag.
+    api: Optional[HttpUrl] = None
+    service_name: Optional[str] = None # not actually works, just as a tag.
 
+@dataclass
 class OfficialPlayer(OnlinePlayer):
     official_account: bool = field(default=True, init=False)
-    service_name: str = "microsoft"
+    api: Optional[HttpUrl] = "https://api.mojang.com/users/profiles/minecraft"
+    service_name: str = "mojang"
 
 class YggdrasilPlayer(OnlinePlayer):
     official_account: bool = field(default=False, init=False)
@@ -31,3 +42,7 @@ class YggdrasilPlayer(OnlinePlayer):
 class FloodgatePlayer(OnlinePlayer):
     official_account: bool = field(default=False, init=False)
     service_name: str = "floodgate"
+
+# Testing codes.
+player = OfficialPlayer(name="CleMooling", uuid="392cdfd1-0016-4afa-ae13-66488ef79bb9")
+print(player)
