b08a8fc0cae0743542d6bf20c98ef26340c90282
Mooling0602 <clemooling@outlook.com>
2025-05-17 21:08:59 +0800

    feat: add time tools in utils

--- player_api/utils.py
+++ player_api/utils.py
@@ -1,4 +1,7 @@
-from typing import Callable, Any
+import locale
+
+from typing import Any, Callable, Optional
+from datetime import datetime, timezone
 
 
 # Usage: @execute_if(bool | Callable -> bool)
@@ -15,3 +18,28 @@ def execute_if(condition: bool | Callable[[], bool]):
             return None
         return wrapper
     return decorator
+
+
+def get_time(return_str: Optional[bool]) -> datetime | str:
+    if not return_str:
+        return datetime.now(timezone.utc)
+    else:
+        return datetime.now(timezone.utc).isoformat()
+
+
+def get_time_styled(locale: Optional[str]) -> str:
+    format = r"%Y-%m-%d %H:%M:%S %A"
+    if locale == "zh_cn":
+        locale.setlocale(locale.LC_TIME, 'zh_CN.UTF-8')
+        format = r"%Y年%m月%d日 %H:%M:%S %A"
+    return datetime.now().astimezone().strftime(format)
+
+
+def format_time(time: datetime | str, locale: Optional[str]) -> str:
+    format = r"%Y-%m-%d %H:%M:%S %A"
+    if locale == "zh_cn":
+        locale.setlocale(locale.LC_TIME, 'zh_CN.UTF-8')
+        format = r"%Y年%m月%d日 %H:%M:%S %A"
+    if not isinstance(time, datetime):
+        time = datetime.fromisoformat(time)
+    return time.astimezone().strftime(format)
